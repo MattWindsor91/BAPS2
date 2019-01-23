c37d6ca1ed2cc54e8d0abf1cfd6b2124e072ec86
Matt Windsor <mattwindsor@btinternet.com>
2019-01-23 23:29:25 +0000

    Add and use header custom-control
    This DRYs up the header controls on the channel and directory bins.

---
+++ BAPSPresenterNG/Header.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BAPSPresenterNG
+{
+    /// <summary>
+    /// A control used for the headers of BAPS channels and directories.
+    /// <para>
+    ///   The header control contains three elements:
+    ///   <list type="bullet">
+    ///     <item>a FontAwesome icon (<see cref="Icon"/>);</item>
+    ///     <item>a text legend (<see cref="Text"/>);</item>
+    ///     <item>
+    ///       a content section intended for buttons or icons
+    ///       (the control's body).
+    ///     </item>
+    ///   </list>
+    /// </para>
+    /// </summary>
+    public class Header : ContentControl
+    {
+        static Header()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Header), new FrameworkPropertyMetadata(typeof(Header)));
+        }
+
+        #region DependencyProperty Text
+
+        /// <summary>
+        /// Registers a dependency property as backing store for the Text property
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(Header),
+            new FrameworkPropertyMetadata("",
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsParentMeasure));
+
+        /// <summary>
+        /// Gets or sets the icon.
+        /// </summary>
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set { SetValue(TextProperty, value); }
+        }
+
+        #endregion DependencyProperty Text
+
+        #region DependencyProperty Icon
+
+        /// <summary>
+        /// Registers a dependency property as backing store for the Icon property
+        /// </summary>
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(FontAwesome.WPF.FontAwesomeIcon), typeof(Header),
+            new FrameworkPropertyMetadata(FontAwesome.WPF.FontAwesomeIcon.Question,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsParentMeasure));
+
+        /// <summary>
+        /// Gets or sets the icon.
+        /// </summary>
+        public FontAwesome.WPF.FontAwesomeIcon Icon
+        {
+            get => (FontAwesome.WPF.FontAwesomeIcon)GetValue(IconProperty);
+            set { SetValue(IconProperty, value); }
+        }
+
+        #endregion DependencyProperty Icon
+    }
+}
