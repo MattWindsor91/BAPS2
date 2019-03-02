3c5d69f4e5926a4a7d7e3df2851de01ba9532826
Matt Windsor <mattwindsor@btinternet.com>
2019-03-02 19:32:58 +0000

    Separate TrackLegend

---
+++ URY.BAPS.Client.Wpf/Controls/TrackLegend.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace URY.BAPS.Client.Wpf.Controls
+{
+    public partial class TrackLegend
+    {
+        public TrackLegend()
+        {
+            InitializeComponent();
+        }
+    }
+}
