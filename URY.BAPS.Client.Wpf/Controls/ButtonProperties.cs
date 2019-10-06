19c06cfdfd6a1a7012a3a844581bb0f680af283e
Matt Windsor <mattwindsor@btinternet.com>
2019-10-06 20:13:11 +0100

    Try clean/smarten up header buttons
    This is still a work in progress: certainly the channel lamps aren't quite right yet.
    
    Part of this accidentally went in with the last commit.  Oops.

---
+++ URY.BAPS.Client.Wpf/Controls/ButtonProperties.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using FontAwesome5;
+
+namespace URY.BAPS.Client.Wpf.Controls
+{
+    /// <summary>
+    ///     Attached properties used to attach icons and other such things to
+    ///     buttons.
+    /// </summary>
+    public static class ButtonProperties
+    {
+        #region Icon property
+
+        public static EFontAwesomeIcon GetIcon(DependencyObject obj)
+        {
+            return (EFontAwesomeIcon) obj.GetValue(IconProperty);
+        }
+
+        public static void SetIcon(DependencyObject obj, EFontAwesomeIcon value)
+        {
+            obj.SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.RegisterAttached("Icon", typeof(EFontAwesomeIcon), typeof(ButtonProperties),
+                new UIPropertyMetadata(EFontAwesomeIcon.Solid_QuestionCircle));
+
+        #endregion Icon property
+    }
+}
